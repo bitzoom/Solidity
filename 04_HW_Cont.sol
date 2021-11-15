948435d2fb18d361cd0edf0fa68588351199ce8a
bitzoom <93945451+bitzoom@users.noreply.github.com>
2021-11-15 22:55:09 +0400

    Update 04_HW_Cont.sol

--- 04_HW_Cont.sol
+++ 04_HW_Cont.sol
@@ -1,1 +1,28 @@
-Solidity
+// SPDX-License-Identifier: unlicensed
+
+pragma solidity >=0.8.0;
+
+contract VolcanoCoin {
+    
+    uint maxcoins = 10000;
+    address owner;
+    event MaxCoins_set(uint);
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+    if(msg.sender == owner) {
+        _;
+    }
+}
+    function getmaxsupply() public view returns(uint) {
+        return maxcoins;
+    }
+    
+    function increasesupply() public onlyOwner{
+        maxcoins = maxcoins + 1000;
+        emit MaxCoins_set(maxcoins);
+    }
+}
